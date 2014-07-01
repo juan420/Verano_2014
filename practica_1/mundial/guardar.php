c3a215ab4072d4127dd6542d3bfac6e614d0636f
juan420 <lara_zero16@hotmail.com>
2014-07-01 16:51:55 -0500

    practica1

---
+++ practica_1/mundial/guardar.php
@@ -0,0 +1,23 @@
+
+
+<?php
+ echo "hola";
+ echo "goles Equipo local:" . $_POST['equipo_1'];
+ echo "<br />";
+ echo "goles Equipo Visitante:" . $_POST['equipo_2'];
+ 
+ echo "<br />";
+ echo "goles Equipo local:" . $_POST['equipo_3'];
+ echo "<br />";
+ echo "goles Equipo Visitante:" . $_POST['equipo_4'];
+ 
+echo "<br />";
+echo "equipo elegido" . $_POST['equipos_7'];
+ echo "goles Equipo local:" . $_POST['equipo_5'];
+ echo "<br />";
+ echo "equipo elegido" . $_POST['equipos_8'];
+
+ echo "goles Equipo Visitante:" . $_POST['equipo_6'];
+
+
+?>
